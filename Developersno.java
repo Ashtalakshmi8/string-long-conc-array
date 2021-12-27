a50eceed31e8562340d5a60e4c7fd17563856346
Ashtalakshmi8 <ASHTALAKSHMI8>
2021-12-27 13:24:37 +0530

    string-long-con-arrays-for-cs

---
+++ Developersno.java
@@ -0,0 +1,6 @@
+class Developersno{
+public static void main (String [] args){
+long[]ph={8431935331l,7892564006l,7975780647l,8762902478l,9591931134l,9902436835l,7338144269l};
+System.out.println(ph[0]+" "+ph[1]+" "+ph[2]+" "+ph[3]+" "+ph[4]+" "+ph[5]+" "+ph[6]);
+}
+}
---
+++ Friendsno.java
@@ -0,0 +1,9 @@
+class Friendsno{
+public static void main (String [] args){
+long[]ph={8431935331l,7892564006l,7975780647l,8762902478l,9591931134l,9902436835l,7338144269l};
+for (int cell=0;cell<ph.length;cell=cell+1)
+{
+System .out.println(ph[cell]);
+}
+}
+}
